0e60d9aef0c98638eb2b9d4291b697941c4f42ad
Michael Alex <michaelaleks.dev@gmail.com>
2020-06-24 18:21:09 +0400

    worked on converting geoJson to wktString to pass the string to the map

--- src/app/vectorial-shapefile/vectorial-shapefile.component.ts
+++ src/app/vectorial-shapefile/vectorial-shapefile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 declare var shp: any;
@@ -10,6 +10,9 @@ declare var shp: any;
 })
 export class VectorialShapefileComponent implements OnInit {
   vectorialShapefileForm: FormGroup;
+
+  @ViewChild('olMap')
+  olMap: any;
 
   constructor() {}
 
@@ -31,6 +34,20 @@ export class VectorialShapefileComponent implements OnInit {
     });
   }
 
+  convert(input_json) {
+    //convert the json-input to WKT
+    var wkt_str =
+      'POLYGON((' +
+      input_json.coordinates.map((ring, index) => {
+        console.log('ring', ring, ring[0], ring[1]);
+        return ring[0] + ' ' + ring[1];
+      }) +
+      '))';
+
+    console.log('wkt_str', wkt_str);
+    return wkt_str;
+  }
+
   add() {
     console.log(this.vectorialShapefileForm.value);
   }
@@ -40,6 +57,13 @@ export class VectorialShapefileComponent implements OnInit {
     reader.addEventListener('load', (event) => {
       shp(event.target.result).then((data) => {
         console.log('data', data); // you should stirgigy this. es unda gastringo
+        const geoJson = {
+          type: data.features[0].geometry.type,
+          coordinates: [...data.features[0].geometry.coordinates[0]],
+        };
+        console.log('>>>>>>', geoJson);
+        const wktText = this.convert(geoJson);
+        this.olMap.drawWkt(wktText);
       });
     });
     reader.readAsArrayBuffer(e.target.files[0]);
